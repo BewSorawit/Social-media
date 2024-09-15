3950d6adddcdc90a72fdbafca2dd20fac03663aa
Lucien <Omegajoker.18@gmail.com>
2024-09-15 15:01:34 +0700

    test

--- frontend-media/src/index.js
+++ frontend-media/src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import ProfilePage from './components/profile/ProfilePage.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import EditProfileModal from './components/profile/EditProfileModal.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ProfilePage />
-    {/* <EditProfileModal/> */}
+    <App/>
   </React.StrictMode>
 );
 
