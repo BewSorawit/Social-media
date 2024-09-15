e170b27fe8681648a090f665719b601da85a61f8
Lucien <Omegajoker.18@gmail.com>
2024-09-15 15:08:41 +0700

    edit App and index
    edit App and index

--- frontend-media/src/index.js
+++ frontend-media/src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+// import App from './App';
+import reportWebVitals from './reportWebVitals';
+import ProfilePage from './components/profile/ProfilePage.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import EditProfileModal from './components/profile/EditProfileModal.js';
+import './components/feed/FeedPage.css'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import FeedPage from './components/feed/FeedPage.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
