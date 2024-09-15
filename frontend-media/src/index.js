6e3680aa51a9ab70aa0acb0599d071bda0aa7a4e
Surachai2545 <156535462+Surachai2545@users.noreply.github.com>
2024-09-15 15:42:05 +0700

    Feed6

--- frontend-media/src/index.js
+++ frontend-media/src/index.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+// import App from './App';
+import reportWebVitals from './reportWebVitals';
+import ProfilePage from './components/profile/ProfilePage.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import EditProfileModal from './components/profile/EditProfileModal.js';
+import './components/feed/FeedPage.css'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
 import FeedPage from './components/feed/FeedPage.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <FeedPage />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
