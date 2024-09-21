b809b2d34b669cdd82c3c37c27ca2847db865fe0
SaranLilly <117520812+SaranLilly@users.noreply.github.com>
2024-09-22 01:21:31 +0700

    add api post

--- BackendMedia/posts/urls.py
+++ BackendMedia/posts/urls.py
@@ -5,13 +5,20 @@ urlpatterns = [
 
     # เห็นโพสต์ทั้งหมดที่เป็น public
     path('public/', PostViewSet.as_view({'get': 'list_public'})),
+    path('private/', PostViewSet.as_view({'get': 'list_private'})),
+
     # แสดงรายการโพสต์ public โดยผู้เขียนคนใดคนหนึ่ง
     path('public/by_author/<int:author_id>/',
          PostViewSet.as_view({'get': 'list_public_by_author'})),
+     path('public_and_private/by_author/<int:author_id>/',
+         PostViewSet.as_view({'get': 'list_public_and_private_by_author'})),
+
     # แสดงรายการโพสต์ทั้งหมดที่ผู้ใช้มองเห็นได้ (สาธารณะและส่วนตัวสำหรับผู้ใช้ที่เข้าสู่ระบบ)
     path('all/', PostViewSet.as_view({'get': 'list_all'})),
+
     # แสดงรายการโพสต์ส่วนตัวที่ผู้ใช้ที่เข้าสู่ระบบมองเห็นได้ (โพสต์ที่ต้องได้รับอนุญาต)
     path('private/', PostViewSet.as_view({'get': 'list_private'})),
+
     path('create/', PostViewSet.as_view({'post': 'create'})),  # สร้างโพสต์
     # อัพเดทและลบโพสต์
     path('<int:pk>/',
