ecee0987abbcf4f8ac9e31e38ad4d54039e11699
Surachai2545 <156535462+Surachai2545@users.noreply.github.com>
2024-09-23 01:13:38 +0700

    susc

--- BackendMedia/posts/serializers.py
+++ BackendMedia/posts/serializers.py
@@ -1,10 +1,18 @@
 from rest_framework import serializers
 from .models import Post
-
+from likes.models import Like  # นำเข้าโมเดล Like
 
 class PostSerializer(serializers.ModelSerializer):
+    like_count = serializers.SerializerMethodField()
+    liked_user_ids = serializers.SerializerMethodField()
+
+    def get_like_count(self, obj):
+        return Like.objects.filter(post=obj).count()
+
+    def get_liked_user_ids(self, obj):
+        return Like.objects.filter(post=obj).values_list('user', flat=True)
+
     class Meta:
         model = Post
-        fields = ['post_id', 'author', 'content',
-                  'image', 'visibility', 'created_at']
+        fields = ['post_id', 'author', 'content', 'image', 'visibility', 'created_at', 'like_count', 'liked_user_ids']
         read_only_fields = ['author', 'created_at']
